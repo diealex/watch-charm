37d60a201aa82df0c2b0dcfb86904fe160dbd321
diealex <serendipisland@gmail.com>
2025-01-12 16:09:45 +0100

    Header menu: link active state update

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ window.showDiv = function() {
 window.hideDiv = function () {
     document.getElementById('mobile-menu').style.display = "none";
 };
-window.setActiveLink = function() {
+window.setActiveLink = function(event) {
   var current = document.getElementsByClassName("active-link");
   if (current.length > 0) { 
     current[0].className = current[0].className.replace(" active-link", "");
   }
   event.currentTarget.className += " active-link";
-  }
+}
