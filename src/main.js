26c29d98d62d9e7291b6f04b07e41e3271c5ebbb
diealex <serendipisland@gmail.com>
2025-01-12 22:22:45 +0100

    mobile-menu: bg added & closing thing expanded

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-window.showDiv = function() {
-    document.getElementById('mobile-menu').style.display = "flex";
+window.showDiv = function(event) {
+  document.getElementById('mobile-menu').style.display = "flex";
+  document.addEventListener('click', onDocumentClick);
+  event.stopPropagation();
 };
 window.hideDiv = function () {
-    document.getElementById('mobile-menu').style.display = "none";
+  document.getElementById('mobile-menu').style.display = "none";
+  document.removeEventListener('click', onDocumentClick)
 };
 window.setActiveLink = function(event) {
   var current = document.getElementsByClassName("active-link");
@@ -10,4 +13,8 @@ window.setActiveLink = function(event) {
     current[0].classList.remove("active-link");
   }
   event.currentTarget.classList.add("active-link");
+}
+var mobileMenu = window.document.getElementById('mobile-menu');
+window.onDocumentClick = function (event) {
+  if (!mobileMenu.contains(event.target)) hideDiv();
 }
