0ea9f328a51154bff044609b0b4238e5c7e1d092
diealex <serendipisland@gmail.com>
2025-01-12 16:04:57 +0100

    Active state for links in main/mobile menu added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 window.showDiv = function() {
     document.getElementById('mobile-menu').style.display = "flex";
 };
-window.hideDiv = function() {
+window.hideDiv = function () {
     document.getElementById('mobile-menu').style.display = "none";
-}
+};
+window.setActiveLink = function() {
+  var current = document.getElementsByClassName("active-link");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active-link", "");
+  }
+  event.currentTarget.className += " active-link";
+  }
